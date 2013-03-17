25f35f8caed39d9f208ebd46c5784367da93f3cc
Andrew Borghesani <coolkid999@gmail.com>
2013-03-17 14:28:15 -0400

    Slider show up
    see if you can get it to work

--- GUI.java
+++ GUI.java
@@ -2,18 +2,21 @@
  * @author Andrew Borghesani
  */
 
+//ANDREW, i got the slider to show up, but i have no idea how to get it to do anything XD can you help?
+
 import java.awt.*;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 import javax.swing.*;
 import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 public class GUI extends JFrame{
 
-	static final int Speed_Min = 1;
-	static final int Speed_Max = 1000;
-	static final int Speed_Init = 100;
+	static final int Speed_Min = 0;
+	static final int Speed_Max = 100;
+	static final int Speed_Init = 10;
 	double rotateSpeed = .01;
 	int delay;
 	JButton run;
@@ -42,7 +45,8 @@ public class GUI extends JFrame{
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.stepButton();
 		this.playButton();
-//		rotateSlider.add(speedSlider());
+		rotateSlider.add(speedSlider());
+		getContentPane().add(rotateSlider,BorderLayout.EAST);
 		setVisible(true);
 		p = new Paint(shapes,l);
 		add(p);
@@ -50,23 +54,23 @@ public class GUI extends JFrame{
 		p.repaint();
 		setSize(800,600);
 	}
-	public void addGridBag(){
-		GridBagLayout gridBag = new GridBagLayout();
-		GridBagConstraints c = new GridBagConstraints();
-        setFont(new Font("SansSerif", Font.PLAIN, 14));
-        setLayout(new BorderLayout());
-        c.fill = GridBagConstraints.BOTH;
-        c.weightx = 1.0;/*
-        add("Run", gridBag, c);
-        makebutton("Button2", gridBag, c);
-        makebutton("Button3", gridBag, c);*/
-
-	}
+//	public void addGridBag(){
+//		GridBagLayout gridBag = new GridBagLayout();
+//		GridBagConstraints c = new GridBagConstraints();
+//        setFont(new Font("SansSerif", Font.PLAIN, 14));
+//        setLayout(new BorderLayout());
+//        c.fill = GridBagConstraints.BOTH;
+//        c.weightx = 1.0;/*
+//        add("Run", gridBag, c);
+//        makebutton("Button2", gridBag, c);
+//        makebutton("Button3", gridBag, c);*/
+//
+//	}
 	public void stateChanged(ChangeEvent e) {
 		JSlider source = (JSlider)e.getSource();
-		if (!source.getValueIsAdjusting()) {
+		if (source.getValueIsAdjusting()) {
 			int speed = (int)source.getValue();
-			rotateSpeed= speed/100;
+			rotateSpeed= speed/100.0;
 		}
 	}
 	//creates a button that steps to the next generation
@@ -93,21 +97,24 @@ public class GUI extends JFrame{
 		});
 		getContentPane().add(run,BorderLayout.SOUTH);
 	}
-	public void speedSlider(){
+	public JSlider speedSlider(){
 		JLabel sliderLabel = new JLabel("Rotate Speed");
-		JSlider Speed = new JSlider(JSlider.HORIZONTAL,Speed_Min, Speed_Max, Speed_Init);
-		sliderLabel.setAlignmentX(LEFT_ALIGNMENT);
-		Speed.setMajorTickSpacing(100);
-		Speed.setMinorTickSpacing(10);
+		JSlider Speed = new JSlider(JSlider.VERTICAL,Speed_Min, Speed_Max, Speed_Init);
+		sliderLabel.setAlignmentX(CENTER_ALIGNMENT);
+//		It has to do with this line, IDK what a changelistener is
+        Speed.addChangeListener((ChangeListener) this);
+		Speed.setMajorTickSpacing(10);
+		Speed.setMinorTickSpacing(1);
 		Speed.setPaintTicks(true);
 		Speed.setPaintLabels(true);
 		Speed.setBorder(
-				BorderFactory.createEmptyBorder(0,0,10,0));
+				BorderFactory.createEmptyBorder());
 		Font font = new Font("Serif", Font.ITALIC, 15);
 		Speed.setFont(font);
 		add(sliderLabel);
 		add(Speed);
-		Speed.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+		Speed.setBorder(BorderFactory.createEmptyBorder(1,1,1,1));
+		return Speed;
 	}
 	//tells the step button what to do
 	private void stepButtonActionPerformed(){
